aa6a9c75a33a362ad80768af8f87a2b0934af533
bivashy <botyrbojey@gmail.com>
2024-08-08 00:39:27 +0500

    Add ResilienceConfiguration

---
+++ telegram-bot/src/main/java/io/github/bivashy/wttj/telegram/bot/configuration/ResilienceConfiguration.java
@@ -0,0 +1,42 @@
+package io.github.bivashy.wttj.telegram.bot.configuration;
+
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
+import io.github.resilience4j.retry.RetryConfig;
+import io.github.resilience4j.retry.RetryRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class ResilienceConfiguration {
+
+    @Bean
+    public RetryConfig retryConfig() {
+        return RetryConfig.custom()
+                .maxAttempts(3)
+                .waitDuration(Duration.ofMillis(500))
+                .failAfterMaxAttempts(true)
+                .build();
+    }
+
+    @Bean
+    public RateLimiterConfig rateLimiterConfig() {
+        return RateLimiterConfig.custom()
+                .limitRefreshPeriod(Duration.ofSeconds(5))
+                .limitForPeriod(1)
+                .build();
+    }
+
+    @Bean
+    public RateLimiterRegistry rateLimiterRegistry(RateLimiterConfig config) {
+        return RateLimiterRegistry.of(config);
+    }
+
+    @Bean
+    public RetryRegistry retryRegistry(RetryConfig config) {
+        return RetryRegistry.of(config);
+    }
+
+}
